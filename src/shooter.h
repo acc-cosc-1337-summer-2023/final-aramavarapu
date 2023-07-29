2e3f58520d63f8ba563b2ec6f884f3dbc01469c1
aramavarapu <135273286+aramavarapu@users.noreply.github.com>
2023-07-29 05:12:40 +0000

    Question 3

--- src/shooter.h
+++ src/shooter.h
@@ -1,1 +1,22 @@
 //
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include "roll.h"
+
+#ifndef SHOOTER_H
+#define SHOOTER_H
+
+class Shooter 
+{
+public:
+Roll* throw_die(Die& die1, Die& die2);
+~Shooter();
+friend std::ostream& operator<<(std::ostream& os, const Shooter& shooter);
+
+private:
+std::vector<Roll*> rolls;
+};
+
+#endif
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -2,10 +2,14 @@
 #include "catch.hpp"
 #include "die.h"
 #include "roll.h"
+#include "shooter.h"
+#include "phase.h"
+#include "point_phase.h"
+#include "come_out_phase.h"
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
-TEST_CASE("Verify Die Roll Values", "verfication") {
+TEST_CASE("Test die rolls return a value from 1 to 6", "verfication") {
     Die dice;
     for (int i = 0; i < 10; ++i) {
         dice.roll();
@@ -14,7 +18,7 @@ TEST_CASE("Verify Die Roll Values", "verfication") {
         REQUIRE(result <= 6);
     }
 }
-TEST_CASE("Die Roll Returns Value from 2 to 12", "verfication") {
+TEST_CASE("Test die rolls return a value from 2 to 12", "verfication") {
 Die die1, die2;
 Roll roll(die1, die2);
     for (int i = 0; i < 10; ++i) {
@@ -23,4 +27,29 @@ Roll roll(die1, die2);
         REQUIRE(result >= 2);
         REQUIRE(result <= 12);
     }
+}
+TEST_CASE("Test that ComeOutPhase get outcomes returns values RollOutcome::natural, RollOutcome::craps, and RollOutcome::point", "verfication") {
+    Die die1, die2;
+    ComeOutPhase comeOutPhase;
+    
+    Roll roll1(die1, die2);
+    roll1.roll_die();
+    int rollValue = roll1.roll_value();
+    REQUIRE((rollValue == 7 || rollValue == 11));
+    RollOutcome outcome = comeOutPhase.get_outcome(&roll1);
+    REQUIRE(outcome == RollOutcome::natural);
+
+    Roll roll2(die1, die2);
+    roll2.roll_die();
+    rollValue = roll2.roll_value();
+    REQUIRE((rollValue == 2 || rollValue == 3 || rollValue == 12)); 
+    outcome = comeOutPhase.get_outcome(&roll2);
+    REQUIRE(outcome == RollOutcome::craps);
+
+    Roll roll3(die1, die2);
+    roll3.roll_die();
+    rollValue = roll3.roll_value();
+    REQUIRE(!(rollValue == 2 || rollValue == 3 || rollValue == 7 || rollValue == 11 || rollValue == 12));
+    outcome = comeOutPhase.get_outcome(&roll3);
+    REQUIRE(outcome == RollOutcome::point);
 }
